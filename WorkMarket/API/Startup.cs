1c173ee1aa42fc3c5c4861aa26e956d44a77baf4
Gohar Aghababyan <goharaghababyan@gmail.com>
2016-10-08 16:05:46 +0400

    Init WorkMarket project: Web API config

---
+++ WorkMarket/API/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WorkMarket.API.Startup))]
+namespace WorkMarket.API
+{
+    public class Startup
+    {
+        public void Configure(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
